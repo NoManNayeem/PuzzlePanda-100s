38d1bdb498037480abb2a8fcc81a371184bb8d5a
NoManNayeem <nayeem60151126@gmail.com>
2024-05-20 12:08:46 +0600

    updated Subscribe Page

--- src/app/user/subscribe/page.js
+++ src/app/user/subscribe/page.js
@@ -69,69 +69,76 @@ const SubscribePage = () => {
   };
 
   return (
-    <div className="min-h-screen flex flex-col bg-gradient-to-r from-blue-100 to-purple-200">
+    <div className="min-h-screen flex flex-col bg-gradient-to-r from-indigo-200 via-purple-200 to-pink-200">
       <PrivateHeader />
       <PrivateNavBar />
       <div className="flex-grow flex flex-col items-center justify-center p-6">
-        <h1 className="text-4xl font-bold text-center mb-8 text-gray-800">Subscribe to PuzzlePanda!</h1>
-        <div className="grid grid-cols-1 gap-6 w-full max-w-md">
-          <div className="bg-white rounded-lg shadow-xl p-6 flex flex-col items-center text-center transform hover:scale-105 transition-transform">
-            <img src={digimartOption.logo} alt={`${digimartOption.name} Logo`} className="w-32 h-32 mb-6 object-contain" />
-            {!redirectUrl ? (
-              <>
+        <div className="w-full max-w-md bg-white rounded-lg shadow-xl p-8 flex flex-col items-center text-center transform hover:scale-105 transition-transform duration-300 ease-in-out">
+          <img src={digimartOption.logo} alt={`${digimartOption.name} Logo`} className="w-62 h-28 mb-2 object-contain" />
+          {!redirectUrl ? (
+            <>
+              <div className="flex items-center p-4 mb-2 text-sm text-blue-900 border border-blue-300 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400 dark:border-blue-800" role="alert">
+                <svg className="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                  <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+                </svg>
+                <span className="sr-only">Info</span>Subscribe with Grameenphone!
+              </div>
+
+              <button
+                className="bg-blue-500 text-white font-semibold px-6 py-3 rounded-full shadow-md hover:bg-blue-600 transition duration-300 mb-4"
+                onClick={handleSubscribeNow}
+                disabled={loading}
+                aria-busy={loading}
+              >
+                {loading ? (
+                  <div className="loader"></div>
+                ) : (
+                  `Subscribe Now with ${digimartOption.name}`
+                )}
+              </button>
+              <form onSubmit={handleSubscribeWithPhone} className="w-full flex ">
+                <input
+                  type="text"
+                  value={phone}
+                  onChange={(e) => setPhone(e.target.value)}
+                  className="border border-gray-300 rounded-l-full text-black px-4 py-2 w-full shadow-sm focus:ring-2 focus:ring-blue-400"
+                  placeholder="Subscribe with Different Phone"
+                  aria-label="Phone number"
+                />
                 <button
-                  className="bg-blue-600 text-white px-6 py-3 rounded-full shadow-lg hover:bg-blue-700 transition-colors mb-4"
-                  onClick={handleSubscribeNow}
+                  type="submit"
+                  className="bg-green-500 text-white text-xs font-semibold px-6 py-2 rounded-r-full shadow-md hover:bg-green-600 transition duration-300"
                   disabled={loading}
+                  aria-busy={loading}
                 >
                   {loading ? (
                     <div className="loader"></div>
                   ) : (
-                    `Subscribe Now with ${digimartOption.name}`
+                    `Subscribe`
                   )}
                 </button>
-                <form onSubmit={handleSubscribeWithPhone} className="w-full flex items-center space-x-2">
-                  <input
-                    type="text"
-                    value={phone}
-                    onChange={(e) => setPhone(e.target.value)}
-                    className="border rounded-full text-black px-4 py-3 w-full shadow-md focus:ring-2 focus:ring-blue-600"
-                    placeholder="Subscriber With Different Phone"
-                  />
-                  <button
-                    type="submit"
-                    className="bg-green-600 text-white px-6 py-3 rounded-full shadow-lg hover:bg-green-700 transition-colors"
-                    disabled={loading}
-                  >
-                    {loading ? (
-                      <div className="loader"></div>
-                    ) : (
-                      `Subscribe`
-                    )}
-                  </button>
-                </form>
-              </>
-            ) : showContinueButton ? (
-              <Link
-                href={redirectUrl}
-                passHref
-                className="bg-green-600 text-white px-6 py-3 rounded-full shadow-lg hover:bg-green-700 transition-colors"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Continue
-              </Link>
-            ) : (
-              <button
-                className="bg-gray-500 text-white px-6 py-3 rounded-full shadow-lg"
-                disabled
-              >
-                Preparing link...
-              </button>
-            )}
-          </div>
+              </form>
+            </>
+          ) : showContinueButton ? (
+            <Link
+              href={redirectUrl}
+              passHref
+              className="bg-green-500 text-white font-semibold px-6 py-3 rounded-full shadow-md hover:bg-green-600 transition duration-300"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Continue
+            </Link>
+          ) : (
+            <button
+              className="bg-gray-500 text-white font-semibold px-6 py-3 rounded-full shadow-md"
+              disabled
+            >
+              Preparing link...
+            </button>
+          )}
+          {error && <p className="text-red-500 mt-4">{error}</p>}
         </div>
-        {error && <p className="text-red-500 mt-4">{error}</p>}
       </div>
       <PrivateFooter />
       <style jsx>{`
